d02410723a27fb142d7b8eaa85ab29856f11197f
antoniogv-dev <antoniogutierrezvaras@gmail.com>
2026-09-02 14:21:01 -0400

    Fix v12.1.1: remove duplicate legacy D1 migration 0014

--- migrations/0014_curriculum_graduation_sections.sql
+++
@@ -1,3 +0,0 @@
--- v12.1.1 · Malla curricular: asignaturas de graduación y secciones particulares por cohorte.
--- CurriculumCourseKind es representado como TEXT en SQLite, por lo que GRADUACION no requiere ALTER TYPE.
-ALTER TABLE "CohortBudget" ADD COLUMN "courseSectionOverrides" TEXT NOT NULL DEFAULT '{}';
